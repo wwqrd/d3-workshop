955353a7a41eb9b7986c267a1f8dceb1d78bd6e8
Steve Mckellar <stevemckellar@gmail.com>
2013-02-09 23:28:24 +0000

    Fixed up formatting.

--- Examples/js/visualisation_graph.js
+++ Examples/js/visualisation_graph.js
@@ -1,42 +1,50 @@
 (function( d3 ) {
 
+    var svg,
+        xScale,
+        yScale,
+        border = 25;
+        width = 800,
+        height = 600;
+
     // Create a workspace
-    var svg = d3.select('#visualisation')
-        .append('svg')
-        .attr('width', '100%')
-        .attr('height', '100%');
+    svg = d3.select( '#visualisation' )
+        .append( 'svg' )
+        .attr( 'width', width )
+        .attr( 'height', height );
 
     // Loading data
-    d3.csv('data/titanic.csv', function( data ) {
-        var xScale = d3.scale.linear()
+    d3.csv( 'data/titanic.csv', function( data ) {
+
+        xScale = d3.scale.linear()
             .domain([
                 0,
-                d3.max(data, function( d ) { return parseFloat(d.age, 10); })
+                d3.max( data, function( d ) { return parseFloat( d.age, 10 ); })
             ])
-            .range(['5%', '95%']);
+            .range([ border, width - border ]);
 
-        var yScale = d3.scale.linear()
+        yScale = d3.scale.linear()
             .domain([
                 1,
                 3
             ])
-            .range(['25%', '75%']);
+            .range([ height * 0.25, height * 0.75 ]);
 
-        svg.selectAll('.person')
+        svg.selectAll( '.person' )
             .data( data )
-            .enter().append('circle')
-            .attr('class','person')
-            .style('fill', 'black')
-            .style('opacity', 0.25)
-            .attr('r', 20)
-            .attr('cx', 50)
-            .attr('cy', '50%');
+            .enter().append( 'circle' )
+            .attr( 'class','person' )
+            .style( 'fill', 'black' )
+            .style( 'opacity', 0.25 )
+            .attr( 'r', 20 )
+            .attr( 'cx', 50 )
+            .attr( 'cy', '50%' );
 
         // DOM IS THE DATA
-        svg.selectAll('.person')
-            .attr('cx', function(d) { return xScale(d.age); })
-            .attr('cy', function(d) { return yScale(d.pclass); })
-            .style('fill', function( d ) { return ( d.survived === '1' ? 'blue' : 'red' ); });
+        svg.selectAll( '.person' )
+            .attr( 'cx', function( d ) { return xScale( d.age ); })
+            .attr( 'cy', function( d ) { return yScale( d.pclass ); })
+            .style( 'fill', function( d ) { return ( d.survived === '1' ? 'blue' : 'red' ); });
 
     });
 
